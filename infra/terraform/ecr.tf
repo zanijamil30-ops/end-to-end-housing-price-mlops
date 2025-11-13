e687aea3f6b23122541f1b88a8da6e476ebbd55c
Zainab Jamil <zanijamil30@gmail.com>
2025-11-13 14:54:38 +0500

    Update ecr.tf

--- infra/terraform/ecr.tf
+++ infra/terraform/ecr.tf
@@ -1,1 +1,7 @@
+# ecr.tf — AWS Elastic Container Registry (ECR) setup
+
+resource "aws_ecr_repository" "housing_mlops_repo" {
+  name = "housing-mlops-repository"
+  image_tag_mutability = "MUTABLE"
+}
 
