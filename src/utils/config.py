d12b1b6ebe2972860d2dd4f6e57b1bc3d50d6098
Zainab Jamil <zanijamil30@gmail.com>
2025-11-13 15:12:30 +0500

    Update config.py

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,1 +1,29 @@
+# src/utils/config.py
+import yaml
+
+def load_config(config_path="params.yaml"):
+    """
+    Load the YAML configuration file.
+    """
+    with open(config_path, "r") as file:
+        config = yaml.safe_load(file)
+    return config
+
+def get_model_params(config):
+    """
+    Get model parameters from the config dictionary.
+    """
+    return config.get("model", {})
+
+def get_data_params(config):
+    """
+    Get data-related parameters from the config dictionary.
+    """
+    return config.get("data", {})
+
+def get_logging_params(config):
+    """
+    Get logging-related parameters from the config dictionary.
+    """
+    return config.get("logging", {})
 
