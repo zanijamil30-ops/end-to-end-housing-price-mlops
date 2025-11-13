e1d9fd1dbc44e719ff2794aca155de299382f535
Zainab Jamil <zanijamil30@gmail.com>
2025-11-13 14:55:00 +0500

    Update s3.tf

--- infra/terraform/s3.tf
+++ infra/terraform/s3.tf
@@ -1,1 +1,12 @@
+# s3.tf — S3 Bucket for storing data/models
+
+resource "aws_s3_bucket" "housing_mlops_bucket" {
+  bucket = "housing-mlops-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "housing-mlops-data"
+    Environment = "Production"
+  }
+}
 
